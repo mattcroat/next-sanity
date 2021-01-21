943055d97e0935af1ec4f5ab29e6f534a2152af6
mattcroat <mattcroat.dev@gmail.com>
2021-01-21 14:50:47 +0100

    refactor: Separate YouTube embed schema into components

---
+++ studio/components/YouTubePreview.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { getYouTubeId } from '../helpers'
+
+export function YouTubePreview({ value }) {
+  const id = getYouTubeId(value.url)
+  const url = `https://www.youtube.com/embed/${id}`
+
+  if (!id) {
+    return <div>Missing YouTube URL</div>
+  }
+
+  return (
+    <iframe
+      title="YouTube Preview"
+      width="560"
+      height="315"
+      src={url}
+      frameBorder="0"
+    ></iframe>
+  )
+}
--- studio/schemas/youtube.js
+++ studio/schemas/youtube.js
@@ -1,36 +1,4 @@
-import React from 'react'
-
-function getYouTubeId(url) {
-  const regularUrl = /watch\?v=/.test(url)
-  const shareUrl = /youtu.be/.test(url)
-
-  if (regularUrl) {
-    return url.match(/watch\?v=(.*)/)[1]
-  }
-
-  if (shareUrl) {
-    return url.match(/youtu.be\/(.*)/)[1]
-  }
-}
-
-function YouTubePreview({ value }) {
-  const id = getYouTubeId(value.url)
-  const url = `https://www.youtube.com/embed/${id}`
-
-  if (!id) {
-    return <div>Missing YouTube URL</div>
-  }
-
-  return (
-    <iframe
-      title="YouTube Preview"
-      width="560"
-      height="315"
-      src={url}
-      frameBorder="0"
-    ></iframe>
-  )
-}
+import { YouTubePreview } from '../components/YouTubePreview'
 
 export default {
   name: 'youtube',
