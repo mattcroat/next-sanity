0fbc116f816c48ae17a23f92cb99b24d80030c6a
mattcroat <mattcroat.dev@gmail.com>
2021-01-22 22:30:18 +0100

    fix: Prop names

--- web/components/YouTubePreview/YouTubePreview.jsx
+++ web/components/YouTubePreview/YouTubePreview.jsx
@@ -9,9 +9,9 @@ export function YouTubePreview({ url }) {
       width="560"
       height="315"
       src={`https://www.youtube.com/embed/${videoId}`}
-      frameborder="0"
+      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
+      allowFullScreen
     ></iframe>
   )
 }
