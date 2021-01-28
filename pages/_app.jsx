35d4b9e94a8e327d2f8789ed1ea635a27a746ef2
mattcroat <mattcroat.dev@gmail.com>
2021-01-28 16:44:10 +0100

    refactor: Global styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,57 +1,7 @@
-import { Global, css } from '@emotion/react'
+import { Global } from '@emotion/react'
 
 import { Layout } from '@/root/components/Layout'
-
-const globalStyles = css`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Merriweather', sans-serif;
-    font-size: 18px;
-    color: hsl(0, 0%, 98%);
-    background: hsl(200, 20%, 14%);
-  }
-
-  h1 {
-    font-size: 3rem;
-    margin: 1rem 0;
-  }
-
-  h2 {
-    font-size: 2rem;
-    margin: 3rem 0;
-  }
-
-  p {
-    margin: 2rem 0;
-    line-height: 1.6;
-  }
-
-  img {
-    height: 100%;
-    width: 100%;
-    display: block;
-    margin: 2rem 0;
-    border-radius: 4px;
-  }
-
-  a {
-    color: white;
-  }
-
-  ul {
-    margin: 2rem 0;
-  }
-
-  li {
-    margin: 0.4rem 0;
-    list-style-position: inside;
-  }
-`
+import { globalStyles } from '@/root/styles/global.js'
 
 export default function App({ Component, pageProps }) {
   return (
